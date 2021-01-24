967467c784a0c4d12ccaf660ad316e10801bd9db
sbuitragoh <sbuitragoh@unal.edu.co>
2021-01-24 18:41:07 -0500

    Correction of lines and remove graph lines

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
 import cv2 as cv
 import os
-from src import preproc as pp
+from data import preproc as pp
+from network import model
+from torchvision import transforms as T
+import numpy as np
+import torch
 
 
 def all_images():
 
-    file_path = './src/TIFF Files/'
+    file_path = 'data/TIFF Files/'
     for f in os.listdir(file_path):
         print(f)
         current_image = cv.imread(file_path+f)
         current_image = cv.cvtColor(current_image, cv.COLOR_BGR2GRAY)
         ct_image = pp.preproccess(current_image)
         rtt_img = pp.rotate_image(ct_image)
-        cv.imwrite('./src/TIFF Rotated/'+f, rtt_img)
+        cv.imwrite('./data/TIFF Rotated/'+f, rtt_img)
         lines_in_rotated = pp.line_obtention(rtt_img)
         pp.line_management(img=rtt_img,
                            lines=lines_in_rotated,
@@ -33,7 +37,28 @@ def sample_image(path):
                        path=path[-14:-4])
 
 
+def model_test(line_img):
+    img = cv.imread(line_img, 0)
+    img = cv.bitwise_not(img)
+    img = np.repeat(img[..., np.newaxis], 3, -1)
+    img = pp.normalization(img)
+
+    prep = T.Compose([
+        T.ToTensor(),
+        T.Normalize(mean = [0.485, 0.456, 0.406], std = [0.229, 0.224, 0.225]),
+    ])
+    test_tensor = prep(img)
+    test_batch = test_tensor.unsqueeze(0)
+    modelito = model.make_model(2)
+    h = modelito.forward(test_batch)
+    tensor_2_numpy = np.transpose(h.detach().numpy())
+
+    return 0
+
+
 if __name__ == '__main__':
-    img_path = './src/TIFF Files/rm_149_017.tif'
+    img_path = 'data/TIFF Files/rm_149_017.tif'
     # sample_image(img_path)
-    all_images()
+    # all_images()
+
+    model_test('data/TIFF Lines/rm_149_005/line_2.tif')
---
+++ network/model.py
@@ -0,0 +1,111 @@
+import torch
+import torch.nn as nn
+import numpy as np
+import math
+
+from torchvision.models import resnet101
+from torch.autograd import Variable
+from itertools import groupby
+from pathlib import Path
+
+
+class PositionalEncoding(nn.Module):
+    def __init__(self, d_model, dropout=0.1, max_len=128):
+        super(PositionalEncoding, self).__init__()
+        self.dropout = nn.Dropout(p=dropout)
+
+        pe = torch.zeros(max_len, d_model)
+        position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model))
+        pe[:, 0::2] = torch.sin(position * div_term)
+        pe[:, 1::2] = torch.cos(position * div_term)
+        pe = pe.unsqueeze(0).transpose(0, 1)
+        self.register_buffer('pe', pe)
+
+    def forward(self, x):
+        x = x + self.pe[:x.size(0), :]
+        return self.dropout(x)
+
+
+class Model(nn.Module):
+
+    def __init__(self, vocab_len, hidden_dim, nheads, num_encoder_layers, num_decoder_layers):
+        super().__init__()
+
+        # Structure to get features
+        self.backbone = resnet101()
+        del self.backbone.fc
+
+        self.conv = nn.Conv2d(2048, hidden_dim, 1)
+
+        # Setting Transformer
+        self.transformer = nn.Transformer(
+            hidden_dim, nheads, num_encoder_layers, num_decoder_layers)
+
+        self.vocab = nn.Linear(hidden_dim, vocab_len)
+
+        self.decoder = nn.Embedding(vocab_len, hidden_dim)
+        self.query_pos = PositionalEncoding(hidden_dim, .2)
+
+        self.row_embed = nn.Parameter(torch.rand(50, hidden_dim // 2))
+        self.col_embed = nn.Parameter(torch.rand(50, hidden_dim // 2))
+        self.trg_mask = None
+
+
+    def generate_square_subsequent_mask(self, sz):
+        mask = torch.triu(torch.ones(sz, sz), 1)
+        mask = mask.masked_fill(mask==1, float('-inf'))
+        return mask
+
+
+    def get_features(self, x):
+        x = self.backbone.conv1(x)
+        x = self.backbone.bn1(x)
+        x = self.backbone.relu(x)
+        x = self.backbone.maxpool(x)
+
+        x = self.backbone.layer1(x)
+        x = self.backbone.layer2(x)
+        x = self.backbone.layer3(x)
+        x = self.backbone.layer4(x)
+
+        return x
+
+
+    def make_len_mask(self, inp):
+        return (inp == 0).transpose(0, 1)
+
+
+    def forward(self, inputs, trg):
+        x = self.get_features(inputs)
+        h = self.conv(x)
+
+        bs, _, W, H = h.shape
+        pos = torch.cat([
+            self.col_embed[:W].unsqueeze(0).repeat(H, 1, 1),
+            self.row_embed[:H].unsqueeze(1).repeat(1, W, 1),
+        ], dim=-1).flatten(0, 1).unsqueeze(1)
+
+        # generating subsequent mask for target
+        if self.trg_mask is None or self.trg_mask.size(0) != len(trg):
+            self.trg_mask = self.generate_square_subsequent_mask(trg.shape[1]).to(trg.device)
+
+        # Padding mask
+        trg_pad_mask = self.make_len_mask(trg)
+
+        # Getting postional encoding for target
+        trg = self.decoder(trg)
+        trg = self.query_pos(trg)
+
+        output = self.transformer(pos + 0.1 * h.flatten(2).permute(2, 0, 1), trg.permute(1, 0, 2),
+                                  tgt_mask=self.trg_mask,
+                                  tgt_key_padding_mask=trg_pad_mask.permute(1, 0))
+
+        return self.vocab(output.transpose(0, 1))
+
+
+def make_model(vocab_len, hidden_dim=256, nheads=4,
+               num_encoder_layers=4, num_decoder_layers=4):
+
+    return Model(vocab_len, hidden_dim, nheads,
+               num_encoder_layers, num_decoder_layers)
